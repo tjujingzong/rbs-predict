c4ddb399b8e047349cb3c8dd057026cf9dc6946e
刘京宗 <470699397@qq.com>
2023-06-06 14:16:27 +0800

    add loadmodel

---
+++ LoadModel.py
@@ -0,0 +1,179 @@
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+from sklearn.model_selection import train_test_split
+from torchvision.models.googlenet import GoogLeNet, model_urls
+from torchvision.models.mobilenet import MobileNetV2
+from torchvision.models.mobilenet import mobilenet_v2
+
+
+# dfs遍历所有的碱基组合
+def dfs(current, alphabet, char_map, layer):
+    if len(current) == layer:
+        index = len(char_map)
+        one_hot = [0] * (4 ** layer)
+        one_hot[index] = 1
+        char_map[current] = one_hot
+    else:
+        for base in alphabet:
+            dfs(current + base, alphabet, char_map, layer)
+
+
+# 编码方式
+def encoding(data, encode_type):
+    if encode_type == 'one-hot':
+        char_map = {
+            'A': [1, 0, 0, 0],
+            'T': [0, 1, 0, 0],
+            'C': [0, 0, 1, 0],
+            'G': [0, 0, 0, 1]
+        }
+        seq = []
+        for sequence in data:
+            tmp = [[], [], [], []]
+            for item in sequence:
+                item = item.upper()
+                if item in char_map:
+                    for i in range(4):
+                        tmp[i].append(char_map[item][i])
+                else:
+                    for i in range(4):
+                        tmp[i].append(0)
+            seq.append(tmp)
+        output = torch.tensor(seq, dtype=torch.float32)
+    elif encode_type == 'triplet':
+        alphabet = ['A', 'T', 'C', 'G']
+        char_map = {}
+        dfs("", alphabet, char_map, 3)
+        seq = []
+        for sequence in data:
+            tmp = []
+            for i in range(len(sequence) - 2):  # 仅对序列中的前n-2个碱基进行编码
+                triplet = sequence[i:i + 3].upper()
+                if triplet in char_map:
+                    tmp.append(char_map[triplet])
+                else:
+                    tmp.append([0] * 64)  # 对于无效的三碱基组合，使用全零编码
+            seq.append(tmp)
+        output = torch.tensor(seq, dtype=torch.float32)
+        output = output.permute(0, 2, 1)
+    elif encode_type == 'dimer':
+        char_map = {'GG': [-0.01, -1.78, 3.32, 0.3, 12.1, 32.0, -11.1, -12.2, -29.7, -3.26, 0.17],
+                    'GA': [0.07, -1.7, 3.38, 1.3, 9.4, 32.0, -14.2, -13.3, -35.5, -2.35, 0.1],
+                    'GC': [0.07, -1.39, 3.22, 0.0, 6.1, 35.0, -16.9, -14.2, -34.9, -3.42, 0.26],
+                    'GT': [0.23, -1.43, 3.24, 0.8, 4.8, 32.0, -13.8, -10.2, -26.2, -2.24, 0.27],
+                    'AG': [-0.04, -1.5, 3.3, 0.5, 8.5, 30.0, -14.0, -7.6, -19.2, -2.08, 0.08],
+                    'AA': [-0.08, -1.27, 3.18, -0.8, 7.0, 31.0, -13.7, -6.6, -18.4, -0.93, 0.04],
+                    'AC': [0.23, -1.43, 3.24, 0.8, 4.8, 32.0, -13.8, -10.2, -26.2, -2.24, 0.14],
+                    'AT': [-0.06, -1.36, 3.24, 1.1, 7.1, 33.0, -15.4, -5.7, -15.5, -1.1, 0.14],
+                    'CG': [0.3, -1.89, 3.3, -0.1, 12.1, 27.0, -15.6, -8.0, -19.4, -2.36, 0.35],
+                    'CA': [0.11, -1.46, 3.09, 1.0, 9.9, 31.0, -14.4, -10.5, -27.8, -2.11, 0.21],
+                    'CC': [-0.01, -1.78, 3.32, 0.3, 8.7, 32.0, -11.1, -12.2, -29.7, -3.26, 0.49],
+                    'CT': [-0.04, -1.5, 3.3, 0.5, 8.5, 30.0, -14.0, -7.6, -19.2, -2.08, 0.52],
+                    'TG': [0.11, -1.46, 3.09, 1.0, 9.9, 31.0, -14.4, -7.6, -19.2, -2.11, 0.34],
+                    'TA': [-0.02, -1.45, 3.26, -0.2, 10.7, 32.0, -16.0, -8.1, -22.6, -1.33, 0.21],
+                    'TC': [0.07, -1.7, 3.38, 1.3, 9.4, 32.0, -14.2, -10.2, -26.2, -2.35, 0.48],
+                    'TT': [-0.08, -1.27, 3.18, -0.8, 7.0, 31.0, -13.7, -6.6, -18.4, -0.93, 0.44]}
+        seq = []
+        for sequence in data:
+            tmp = []
+            for i in range(len(sequence) - 1):  # 对序列中的前n-1个碱基进行编码
+                dimer = sequence[i:i + 2].upper()
+                if dimer in char_map:
+                    tmp.append(char_map[dimer])
+                else:
+                    tmp.append([0] * 11)  # 对于无效的二碱基组合，使用全零编码
+            seq.append(tmp)
+        output = torch.tensor(seq, dtype=torch.float32)
+        output = output.permute(0, 2, 1)
+
+    output = output.unsqueeze(1)
+    return output.to(device)
+
+
+# 读取数据
+def read_data(path):
+    dataset = pd.read_csv(path, header=None)
+    x = dataset.iloc[:, 0]
+    y = dataset.iloc[:, 1]
+    l = len(x[0])  # 序列长度
+    x_encoded = encoding(x, encoder)
+    y = torch.tensor(y, dtype=torch.float32).unsqueeze(1)
+
+    # 划分训练集和测试集 当random_state设置为一个固定的整数值时，每次运行train_test_split函数时，数据的分割结果将保持一致。
+    x_train, x_test, y_train, y_test = train_test_split(x_encoded, y, test_size=0.2, random_state=43)
+
+    x_train = x_train.to(device)
+    x_test = x_test.to(device)
+    y_train = y_train.to(device)
+    y_test = y_test.to(device)
+
+    return x_train, y_train, x_test, y_test, l
+
+
+# WrapperGoogleNet
+class WrapperGoogleNet(GoogLeNet):
+    def __init__(self, *args, pretrained=False, **kwargs):
+        super(WrapperGoogleNet, self).__init__(*args, **kwargs)
+        if pretrained:
+            # 下面的这部分代码在 pretrained=True 时加载预训练权重，并去掉了与辅助分类器（auxiliary classifiers）相关的权重
+            state_dict = model_zoo.load_url(model_urls['googlenet'], progress=True)
+            state_dict = {k: v for k, v in state_dict.items() if "aux" not in k}
+            self.load_state_dict(state_dict, strict=False)
+
+
+class GoogleNet(nn.Module):
+    def __init__(self, num_classes, input_channels):
+        super(GoogleNet, self).__init__()
+        self.googlenet = WrapperGoogleNet(pretrained=True, aux_logits=False)
+        # 更改输入通道数 ImageNet 数据集上训练的原始 GoogleNet 模型中的参数设置
+        self.googlenet.conv1 = nn.Conv2d(input_channels, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3))
+        # 使用了一个预训练的 GoogleNet 模型，因此 self.googlenet.fc 的输入维度固定为 1024。
+        self.googlenet.fc = nn.Linear(1024, num_classes)
+
+    def forward(self, x):
+        return self.googlenet(x)
+
+
+class WrapperMobileNet(MobileNetV2):
+    def __init__(self, *args, pretrained=False, **kwargs):
+        super(WrapperMobileNet, self).__init__(*args, **kwargs)
+        if pretrained:
+            pretrained_model = mobilenet_v2(pretrained=True)
+            self.load_state_dict(pretrained_model.state_dict(), strict=False)
+
+
+class MobileNet(nn.Module):
+    def __init__(self, num_classes, input_channels):
+        super(MobileNet, self).__init__()
+        self.mobilenet = WrapperMobileNet(pretrained=True)
+        # 更改输入通道数 并与其他参数的设置与 MobileNetV2 中的一致。
+        self.mobilenet.features[0][0] = nn.Conv2d(input_channels, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1),
+                                                  bias=False)
+        self.mobilenet.classifier[1] = nn.Linear(1280, num_classes)
+
+    def forward(self, x):
+        return self.mobilenet(x)
+
+
+if __name__ == "__main__":
+    encoders = ['one-hot', 'triplet', 'dimer']
+    encoder = encoders[2]
+    data_names = ['rbs', 'promoter', 'rbs1', 'promoter1', 'rbs3']
+    data_name = data_names[2]
+    data_path = r'./data/' + data_name + '-data.csv'
+    model_names = ['GoogleNet', 'MobileNet']
+    model_name = model_names[1]
+    model_path = 'model/' + data_name + '-' + model_name + '_' + encoder + '.pth'
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = MobileNet(num_classes=1, input_channels=1)  # create an instance of the same model class
+    model.to(device)  # move the model to the device
+    model.load_state_dict(torch.load('model/rbs1-MobileNet_dimer.pth'))  # load the parameters from the file
+    model.eval()  # set the model to evaluation mode
+    sequence = "GGAGATGTTTATTATGAAGGAGGATTATCA"
+    encoded_sequence = encoding([sequence], encode_type='dimer')  # encode the sequence into a tensor
+    print(encoded_sequence)
+    intensity = model.forward(encoded_sequence)  # pass the tensor to the model and get the output
+    print(intensity)  # print the predicted intensity
